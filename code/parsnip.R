25623c1eeae79fdd5da36175d67215ebe8d8479e
ais31jp <0jb4f61k3853v2f@gmail.com>
2022-01-04 14:12:19 +0900

    parsnip

---
+++ code/parsnip.R
@@ -0,0 +1,20 @@
+boost_tree(
+  tree_depth = 5,
+  trees = 100,
+  min_n = 30
+) %>% 
+  set_engine("xgboost") %>% 
+  set_mode("classification")
+
+boost_tree_xgboost_spec <-
+  boost_tree(
+    tree_depth = tune(),
+    trees = tune(),
+    learn_rate = tune(),
+    min_n = tune(),
+    loss_reduction = tune(),
+    sample_size = tune(),
+    stop_iter = tune()
+    ) %>%
+  set_engine('xgboost') %>%
+  set_mode('classification')
